afe8c2af89ff222a67cf6b546d16a4c9fff1af1b
AhmedLSayed9 <ahmedelsaayid@gmail.com>
2023-04-26 02:53:04 +0200

    Add naive string search example

---
+++ searching_algorithms/naive_string_search/naive_string_search_example.dart
@@ -0,0 +1,24 @@
+/// Pseudocode:
+/// Loop over the longer string
+/// Loop over the shorter string
+/// If the characters don't match, break out of the inner loop
+/// If the characters do match, keep going
+/// If you complete the inner loop and find a match, increment the count of matches
+/// Return the count
+
+void main() {
+  print(naiveSearch('wowomgzomg', 'omg'));
+}
+
+int naiveSearch(String long, String short) {
+  int totalCount = 0;
+
+  for (int i = 0; i < long.length; i++) {
+    for (int j = 0; j < short.length; j++) {
+      if (i + j == long.length || short[j] != long[i + j]) break;
+      if (j == short.length - 1) totalCount++;
+    }
+  }
+
+  return totalCount;
+}
