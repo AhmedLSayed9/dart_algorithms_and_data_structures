8fb48d6a13ae6ea775fb299f58045bee8ac3cded
AhmedLSayed9 <ahmedelsaayid@gmail.com>
2023-04-29 17:39:41 +0300

    Add insertion sort

---
+++ sorting_algorithms/insertion_sort.dart
@@ -0,0 +1,26 @@
+/// Pseudocode:
+/// - Start by picking the second element in the array.
+/// - Now compare the second element with the one before it and swap if necessary.
+/// - Continue to the next element and if it is in the incorrect order, iterate
+///   through the sorted portion (i.e. the left side) to place the element in the correct place.
+/// - Repeat until the array is sorted.
+///
+/// Time Complexity: Best O(n) - Worst O(n²) - Average O(n²)
+/// Auxiliary Space: O(1)
+
+void main() {
+  print(insertionSort([5, 3, 4, 1, 2]));
+}
+
+List<int> insertionSort(List<int> arr) {
+  for (int i = 1; i < arr.length; i++) {
+    final currentVal = arr[i];
+    int j = i - 1;
+    for (; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    //j + 1 because at last iteration (j--) get executed before checking the condition
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+}
